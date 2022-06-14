1b705cc6dc924d81c9c2861aa15ed2ec1de1ddad
Sangwoo Joo <nooni85@naver.com>
2022-06-14 21:10:51 +0900

    First Commit

---
+++ src/main/java/com/jimiarts/api/JimiartsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jimiarts.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimiartsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimiartsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jimiarts/api/JimiartsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimiarts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimiartsApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
